ca4309df41c40bc6b23186fe3dd4decdcdf36ffa
alanssoares <alan.ssoares@live.com>
2016-03-01 21:26:14 -0300

    atualizando generic dao

--- WebStore/src/br/com/webstore/dao/GenericDao.java
+++ WebStore/src/br/com/webstore/dao/GenericDao.java
@@ -5,9 +5,10 @@ package br.com.webstore.dao;
 
 import java.util.List;
 
-import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 
@@ -17,54 +18,58 @@ import javax.persistence.criteria.CriteriaQuery;
  */
 public abstract class GenericDao<T, I> {
 
-	   @Inject
-	   protected EntityManager entityManager;
+	private static final String NAME_DB = "webstoreDB";
+	
+	protected EntityManager entityManager;
 
-	   private Class<T> persistedClass;
+	private Class<T> persistedClass;
 
-	   protected GenericDao() {}
-
-	   protected GenericDao(Class<T> persistedClass) {
-	       this();
-	       this.persistedClass = persistedClass;
-	   }
-
-	   public T insert(T entity) {
-	       EntityTransaction t = entityManager.getTransaction();
-	       t.begin();
-	       entityManager.persist(entity);
-	       entityManager.flush();
-	       t.commit();
-	       return entity;
-	   }
-
-	   public T update(T entity) {
-	       EntityTransaction t = entityManager.getTransaction();
-	       t.begin();
-	       entityManager.merge(entity);
-	       entityManager.flush();
-	       t.commit();
-	       return entity;
-	   }
-
-	   public void remove(I id) {
-	       T entity = find(id);
-	       EntityTransaction tx = entityManager.getTransaction();
-	       tx.begin();
-	       T mergedEntity = entityManager.merge(entity);
-	       entityManager.remove(mergedEntity);
-	       entityManager.flush();
-	       tx.commit();
-	   }
-
-	   public List<T> getList() {
-	       CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-	       CriteriaQuery<T> query = builder.createQuery(persistedClass);
-	       query.from(persistedClass);
-	       return entityManager.createQuery(query).getResultList();
-	   }
-
-	   public T find(I id) {
-	       return entityManager.find(persistedClass, id);
-	   }
+	protected GenericDao() {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory(NAME_DB);
+		this.entityManager = factory.createEntityManager();
 	}
+
+	protected GenericDao(Class<T> persistedClass) {
+		this();
+		this.persistedClass = persistedClass;
+	}
+
+	public T insert(T entity) {
+		EntityTransaction t = entityManager.getTransaction();
+		t.begin();
+		entityManager.persist(entity);
+		entityManager.flush();
+		t.commit();
+		return entity;
+	}
+
+	public T update(T entity) {
+		EntityTransaction t = entityManager.getTransaction();
+		t.begin();
+		entityManager.merge(entity);
+		entityManager.flush();
+		t.commit();
+		return entity;
+	}
+
+	public void remove(I id) {
+		T entity = find(id);
+		EntityTransaction tx = entityManager.getTransaction();
+		tx.begin();
+		T mergedEntity = entityManager.merge(entity);
+		entityManager.remove(mergedEntity);
+		entityManager.flush();
+		tx.commit();
+	}
+
+	public List<T> getList() {
+		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<T> query = builder.createQuery(persistedClass);
+		query.from(persistedClass);
+		return entityManager.createQuery(query).getResultList();
+	}
+
+	public T find(I id) {
+		return entityManager.find(persistedClass, id);
+	}
+}

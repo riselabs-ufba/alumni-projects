fc0c53a812d3e25ec8d33a5ccbd99a0151c106c4
Walter <Walter@172.20.88.244>
2016-03-03 10:15:41 -0300

    Modificada a estrutura da seguinte forma: * webstoreDB.BugTrack 	idUsuarioRegistro INTEGER UNSIGNED NULL, -- modificado para null * webstoreDB.Mensagem 	idUsuarioRegistro INTEGER UNSIGNED NULL, -- modificado para null * webstoreDB.Venda 	idUsuarioCupom INTEGER UNSIGNED NULL,-- modificado para null

--- WebStore/scripts/Script_WebStore.sql
+++ WebStore/scripts/Script_WebStore.sql
@@ -1,59 +1,62 @@
-CREATE TABLE Categoria (
+/*drop schema webstoreDB;
+  create schema webstoreDB;*/
+
+CREATE TABLE webstoreDB.Categoria (
   idCategoria INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   dsCategoria VARCHAR(255) NULL,
   PRIMARY KEY(idCategoria)
 );
 
-CREATE TABLE Cupom (
+CREATE TABLE webstoreDB.Cupom (
   idCupom INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   dsCupom VARCHAR(255) NULL,
   dtCupomValidade DATETIME NULL,
   PRIMARY KEY(idCupom)
 );
 
-CREATE TABLE FormaPagamento (
+CREATE TABLE webstoreDB.FormaPagamento (
   idFormaPagamento INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   dsFormaPagamento VARCHAR(255) NULL,
   PRIMARY KEY(idFormaPagamento)
 );
 
-CREATE TABLE Perfil (
+CREATE TABLE webstoreDB.Perfil (
   idPerfil INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   dsPerfil VARCHAR(255) NULL,
   PRIMARY KEY(idPerfil)
 );
 
-CREATE TABLE SituacaoBug (
+CREATE TABLE webstoreDB.SituacaoBug (
   idSituacaoBug INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   dsSituacaoBug VARCHAR(255) NULL,
   PRIMARY KEY(idSituacaoBug)
 );
 
-CREATE TABLE StatusUsuario (
+CREATE TABLE webstoreDB.StatusUsuario (
   idStatusUsuario INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   dsStatusUsuario VARCHAR(255) NULL,
   PRIMARY KEY(idStatusUsuario)
 );
 
-CREATE TABLE StatusVenda (
+CREATE TABLE webstoreDB.StatusVenda (
   idStatusVenda INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   dsStatusVenda VARCHAR(255) NULL,
   PRIMARY KEY(idStatusVenda)
 );
 
-CREATE TABLE TipoMensagem (
+CREATE TABLE webstoreDB.TipoMensagem (
   idTipoMensagem INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   dsTipoMensagem VARCHAR(255) NULL,
   PRIMARY KEY(idTipoMensagem)
 );
 
-CREATE TABLE UnidadeMedida (
+CREATE TABLE webstoreDB.UnidadeMedida (
   idUnidadeMedida INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   dsUnidadeMedida VARCHAR(255) NULL,
   PRIMARY KEY(idUnidadeMedida)
 );
 
-CREATE TABLE Usuario (
+CREATE TABLE webstoreDB.Usuario (
   idUsuario INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   idStatusUsuario INTEGER UNSIGNED NOT NULL,
   idPerfil INTEGER UNSIGNED NOT NULL,
@@ -64,11 +67,11 @@ CREATE TABLE Usuario (
   nrUsuarioTelefone VARCHAR(50) NULL,
   dtUsuarioInclusao DATETIME NOT NULL,
   PRIMARY KEY(idUsuario),
-  FOREIGN KEY(idPerfil) REFERENCES Perfil(idPerfil),
-  FOREIGN KEY(idStatusUsuario) REFERENCES StatusUsuario(idStatusUsuario)
+  FOREIGN KEY(idPerfil) REFERENCES webstoreDB.Perfil(idPerfil),
+  FOREIGN KEY(idStatusUsuario) REFERENCES webstoreDB.StatusUsuario(idStatusUsuario)
 );
 
-CREATE TABLE Endereco (
+CREATE TABLE webstoreDB.Endereco (
   idEndereco INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   idUsuario INTEGER UNSIGNED NOT NULL,
   nmEnderecoLogradouro VARCHAR(255) NULL,
@@ -78,35 +81,35 @@ CREATE TABLE Endereco (
   dsEnderecoCidade VARCHAR(255) NULL,
   dsEnderecoUF VARCHAR(255) NULL,
   PRIMARY KEY(idEndereco),
-  FOREIGN KEY(idUsuario) REFERENCES Usuario(idUsuario)
+  FOREIGN KEY(idUsuario) REFERENCES webstoreDB.Usuario(idUsuario)
 );
 
-CREATE TABLE UsuarioCupom (
+CREATE TABLE webstoreDB.UsuarioCupom (
   idUsuarioCupom INTEGER UNSIGNED NOT NULL,
   idCupom INTEGER UNSIGNED NOT NULL,
   idUsuario INTEGER UNSIGNED NOT NULL,
   flUsado BOOL NULL,
   PRIMARY KEY(idUsuarioCupom),
-  FOREIGN KEY(idUsuario) REFERENCES Usuario(idUsuario),
-  FOREIGN KEY(idCupom) REFERENCES Cupom(idCupom)
+  FOREIGN KEY(idUsuario) REFERENCES webstoreDB.Usuario(idUsuario),
+  FOREIGN KEY(idCupom) REFERENCES webstoreDB.Cupom(idCupom)
 );
 
-CREATE TABLE Venda (
+CREATE TABLE webstoreDB.Venda (
   idVenda INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   idStatusVenda INTEGER UNSIGNED NOT NULL,
   idUsuarioVenda INTEGER UNSIGNED NOT NULL,
-  idUsuarioCupom INTEGER UNSIGNED NOT NULL,
+  idUsuarioCupom INTEGER UNSIGNED NULL,-- modificado para null
   idFormaPagamento INTEGER UNSIGNED NOT NULL,
   dtVenda DATETIME NOT NULL,
   vlVendaTotal DECIMAL NOT NULL,
   PRIMARY KEY(idVenda),
-  FOREIGN KEY(idFormaPagamento) REFERENCES FormaPagamento(idFormaPagamento),
-  FOREIGN KEY(idUsuarioVenda) REFERENCES Usuario(idUsuario),
-  FOREIGN KEY(idUsuarioCupom) REFERENCES UsuarioCupom(idUsuarioCupom),
-  FOREIGN KEY(idStatusVenda) REFERENCES StatusVenda(idStatusVenda)
+  FOREIGN KEY(idFormaPagamento) REFERENCES webstoreDB.FormaPagamento(idFormaPagamento),
+  FOREIGN KEY(idUsuarioVenda) REFERENCES webstoreDB.Usuario(idUsuario),
+  FOREIGN KEY(idUsuarioCupom) REFERENCES webstoreDB.UsuarioCupom(idUsuarioCupom),
+  FOREIGN KEY(idStatusVenda) REFERENCES webstoreDB.StatusVenda(idStatusVenda)
 );
 
-CREATE TABLE Produto (
+CREATE TABLE webstoreDB.Produto (
   idProduto INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   idCategoria INTEGER UNSIGNED NOT NULL,
   idUnidadeMedida INTEGER UNSIGNED NOT NULL,
@@ -114,46 +117,46 @@ CREATE TABLE Produto (
   dsProduto VARCHAR(1000) NULL,
   vlProduto DECIMAL NOT NULL,
   PRIMARY KEY(idProduto),
-  FOREIGN KEY(idUnidadeMedida) REFERENCES UnidadeMedida(idUnidadeMedida),
-  FOREIGN KEY(idCategoria) REFERENCES Categoria(idCategoria)
+  FOREIGN KEY(idUnidadeMedida) REFERENCES webstoreDB.UnidadeMedida(idUnidadeMedida),
+  FOREIGN KEY(idCategoria) REFERENCES webstoreDB.Categoria(idCategoria)
 );
 
-CREATE TABLE VendaProduto (
+CREATE TABLE webstoreDB.VendaProduto (
   idProduto INTEGER UNSIGNED NOT NULL,
   idVenda INTEGER UNSIGNED NOT NULL,
   nrQuantidadeProduto DECIMAL NOT NULL,
   vlProdutoUnitario DECIMAL NOT NULL,
   PRIMARY KEY(idProduto, idVenda),
-  FOREIGN KEY(idProduto) REFERENCES Produto(idProduto),
-  FOREIGN KEY(idVenda) REFERENCES Venda(idVenda)
+  FOREIGN KEY(idProduto) REFERENCES webstoreDB.Produto(idProduto),
+  FOREIGN KEY(idVenda) REFERENCES webstoreDB.Venda(idVenda)
 );
 
-CREATE TABLE BugTrack (
+CREATE TABLE webstoreDB.BugTrack (
   idBugTrack INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   idUsuarioResponde INTEGER UNSIGNED NOT NULL,
-  idUsuarioRegistro INTEGER UNSIGNED NOT NULL,
+  idUsuarioRegistro INTEGER UNSIGNED NULL, -- modificado para null
   idSituacaoBug INTEGER UNSIGNED NOT NULL,
   dsBUG VARCHAR(1000) NULL,
   dtRegistro DATETIME NOT NULL,
   dsResposta VARCHAR(1000) NULL,
   dtResposta DATETIME NULL,
   PRIMARY KEY(idBugTrack),
-  FOREIGN KEY(idSituacaoBug) REFERENCES SituacaoBug(idSituacaoBug),
-  FOREIGN KEY(idUsuarioRegistro) REFERENCES Usuario(idUsuario),
-  FOREIGN KEY(idUsuarioResponde) REFERENCES Usuario(idUsuario)
+  FOREIGN KEY(idSituacaoBug) REFERENCES webstoreDB.SituacaoBug(idSituacaoBug),
+  FOREIGN KEY(idUsuarioRegistro) REFERENCES webstoreDB.Usuario(idUsuario),
+  FOREIGN KEY(idUsuarioResponde) REFERENCES webstoreDB.Usuario(idUsuario)
 );
 
-CREATE TABLE Mensagem (
+CREATE TABLE webstoreDB.Mensagem (
   idMensagem INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   idUsuarioResposta INTEGER UNSIGNED NOT NULL,
-  idUsuarioRegistro INTEGER UNSIGNED NOT NULL,
+  idUsuarioRegistro INTEGER UNSIGNED NULL, -- modificado para null
   idTipoMensagem INTEGER UNSIGNED NOT NULL,
   dsMensagem VARCHAR(1000) NULL,
   dtMensagemRegistro DATETIME NOT NULL,
   dsMensagemResposta VARCHAR(1000) NULL,
   dtMensagemResposta DATETIME NULL,
   PRIMARY KEY(idMensagem),
-  FOREIGN KEY(idTipoMensagem) REFERENCES TipoMensagem(idTipoMensagem),
-  FOREIGN KEY(idUsuarioRegistro) REFERENCES Usuario(idUsuario),
-  FOREIGN KEY(idUsuarioResposta) REFERENCES Usuario(idUsuario)
+  FOREIGN KEY(idTipoMensagem) REFERENCES webstoreDB.TipoMensagem(idTipoMensagem),
+  FOREIGN KEY(idUsuarioRegistro) REFERENCES webstoreDB.Usuario(idUsuario),
+  FOREIGN KEY(idUsuarioResposta) REFERENCES webstoreDB.Usuario(idUsuario)
 );

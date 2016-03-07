e6f7c3bcbb40ccb20c33843062df8c36a58d4341
Alan Santos <alan.ssoares@live.com>
2016-03-07 13:02:21 -0300

    Criação de pacote util e classe com constantes

---
+++ WebStore/src/br/com/webstore/utils/Constantes.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package br.com.webstore.utils;
+
+/**
+ * Class with constants used in the project
+ * @author webstore
+ *
+ */
+public class Constantes {
+
+	//Database name
+	public static final String NAME_DB = "webstoreDB";
+}

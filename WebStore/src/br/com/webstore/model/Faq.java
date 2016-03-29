9d73b0e5b2d6001954460841bc8529c4284e8294
ThiagoCunhaUFBA <tiinf@hotmail.com>
2016-03-29 20:04:06 -0300

    Added files via upload
    Classe base de FAQ, feito por Alexandre

---
+++ WebStore/src/br/com/webstore/model/Faq.java
@@ -0,0 +1,68 @@
+//#if ${FAQ} == "T"
+/**
+ *
+ */
+package br.com.webstore.model;
+
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.Table;
+
+/**
+ * @author webstore
+ *
+ */
+@Entity
+@Table(name = "Faq")
+public class Faq {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "idFaq")
+	private Integer id;
+
+	
+	@Column(name = "dsFaq")
+	private String descricao;
+
+	@Column(name = "dsFaqResposta")
+	private String resposta;
+
+
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	
+	public void setId(Integer codigo) {
+		this.id = codigo;
+	}
+	
+
+	public String getDescricao() {
+		return this.descricao;
+	}
+	
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getResposta() {
+		return this.resposta;
+	}
+	
+	public void setResposta(String resposta) {
+		this.resposta = resposta;
+	}
+	
+	
+}
+// #endif

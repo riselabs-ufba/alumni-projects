4db4493e2211b256a7cc9d2cb2dcccb50589f4c8
Allan Oliveira <allanoliveira.main@gmail.com>
2016-04-06 16:10:26 -0300

    fix import on categoria

--- WebStore/src/br/com/webstore/features/CategoriaP.java
+++ WebStore/src/br/com/webstore/features/CategoriaP.java
@@ -15,6 +15,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.table.DefaultTableModel;

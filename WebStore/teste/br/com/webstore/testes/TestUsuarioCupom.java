f5763c77ff455f0bb54d4ed37888bd86f814a24c
Angelaps <angelaps@gmail.com>
2016-04-10 22:31:34 -0300

    Junit UsuarioCupom

--- WebStore/teste/br/com/webstore/testes/TestUsuarioCupom.java
+++ WebStore/teste/br/com/webstore/testes/TestUsuarioCupom.java
@@ -3,6 +3,7 @@
  */
 package br.com.webstore.testes;
 
+import br.com.webstore.model.Usuario;
 import br.com.webstore.model.UsuarioCupom;
 import junit.framework.TestCase;
 
@@ -16,7 +17,13 @@ public class TestUsuarioCupom extends TestCase {
 	 * Test method for {@link br.com.webstore.model.UsuarioCupom#setDisponivel(java.lang.Boolean)}.
 	 */
 	public void testSetDisponivel() {
-
+		UsuarioCupom usuarioCupom = new UsuarioCupom();
+		Boolean disponivel = true;
+		usuarioCupom.getId();
+		usuarioCupom.setDisponivel(disponivel);
+		assertEquals(disponivel, usuarioCupom.getDisponivel());
+		
+		
 	}
 
 }

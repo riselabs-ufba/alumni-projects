3b8a4eb434f18ff8a9dd70b926ff4cc9e5a4bd90
raphamdc <mdc.rapha@gmail.com>
2016-04-11 20:21:36 -0300

    Adição das máscaras e do campo de data de vencimento

--- WebStore/src/br/com/webstore/features/PagamentoCartaoCredito.java
+++ WebStore/src/br/com/webstore/features/PagamentoCartaoCredito.java
@@ -11,6 +11,7 @@ import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.text.MaskFormatter;
 
 import br.com.webstore.facade.GenericFacade;
 import br.com.webstore.model.Usuario;
@@ -19,16 +20,18 @@ import br.com.webstore.model.Usuario;
  * @author webstore
  *
  */
-public class PagamentoCartaoCredito extends JFrame{
-
+public class PagamentoCartaoCredito extends JFrame
+{
 	private static final long serialVersionUID = 4585539280382798864L;
 	
 	private JTextField nomeField;
 	private JFormattedTextField numeroCartaoField;
+	private JFormattedTextField dataVencCartaoField;
 	private JFormattedTextField codigoSegurancaField;
 	
 	
-	public PagamentoCartaoCredito(GenericFacade gfacade, Usuario usuario, String string) {
+	public PagamentoCartaoCredito(GenericFacade gfacade, Usuario usuario, String string) 
+	{
 		final JDialog frame = new JDialog();
 		this.setLayout(null);	
 		this.setTitle("Cartao de Credito");
@@ -50,19 +53,55 @@ public class PagamentoCartaoCredito extends JFrame{
 		lbNumeroCartao.setBounds(2,55,200,15);
 		this.add(lbNumeroCartao);
 		
-		this.numeroCartaoField = new JFormattedTextField();
+		try
+		{
+		    javax.swing.text.MaskFormatter numeroCartaoCredito = new MaskFormatter("####.####.####.####");
+		    this.numeroCartaoField = new JFormattedTextField(numeroCartaoCredito);
+		}
+		catch(Exception e)
+		{
+		};
+		
 		this.numeroCartaoField.setBounds(2, 70, 219, 25);
 		this.numeroCartaoField.setToolTipText("Informe o numero do cartao.");
 		this.numeroCartaoField.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		this.numeroCartaoField.setColumns(10);
 		this.add(this.numeroCartaoField);
 		
+		JLabel lbDataVencimento = new JLabel("Data de Vencimento: ");		
+		lbDataVencimento.setBounds(2,100,200,15);
+		this.add(lbDataVencimento);
+		
+		try
+		{
+		    javax.swing.text.MaskFormatter dataVencimento = new MaskFormatter(" ##/##/####");
+		    this.dataVencCartaoField = new JFormattedTextField(dataVencimento);
+		}
+		catch(Exception e)
+		{
+		};
+		
+		this.dataVencCartaoField.setBounds(2, 115, 219, 25);
+		this.dataVencCartaoField.setToolTipText("Informe o numero do cartao.");
+		this.dataVencCartaoField.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		this.dataVencCartaoField.setColumns(10);
+		this.add(this.dataVencCartaoField);
+		
+		
 		JLabel lbCodigoSeguranca = new JLabel("Codigo de Seguranca: ");		
-		lbCodigoSeguranca.setBounds(2,100,200,15);
+		lbCodigoSeguranca.setBounds(2,145,200,15);
 		this.add(lbCodigoSeguranca);
 		
-		this.codigoSegurancaField = new JFormattedTextField();
-		this.codigoSegurancaField.setBounds(2, 115, 219, 25);
+		try
+		{
+		    javax.swing.text.MaskFormatter codigoSeguranca = new MaskFormatter("###");
+		    this.codigoSegurancaField = new JFormattedTextField(codigoSeguranca);
+		}
+		catch(Exception e)
+		{
+		};
+		
+		this.codigoSegurancaField.setBounds(2, 160, 75, 25);
 		this.codigoSegurancaField.setToolTipText("Informe o numero do cartao.");
 		this.codigoSegurancaField.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		this.codigoSegurancaField.setColumns(10);
@@ -73,9 +112,5 @@ public class PagamentoCartaoCredito extends JFrame{
 		this.add(btFinalizarCompra);
 		
 		this.setVisible(true);
-
 	}
-
-
-
 }

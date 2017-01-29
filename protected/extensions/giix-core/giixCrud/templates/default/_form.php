b86bd76d7df28722222fab74d502aaadd4c7629c
fosales <fosales@gmail.com>
2017-01-28 22:11:17 -0300

    Configuração inicial do sistema.

---
+++ protected/extensions/giix-core/giixCrud/templates/default/_form.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The following variables are available in this template:
+ * - $this: the CrudCode object
+ */
+?>
+<div class="form">
+
+<?php $ajax = ($this->enable_ajax_validation) ? 'true' : 'false'; ?>
+
+<?php echo '<?php '; ?>
+$form = $this->beginWidget('GxActiveForm', array(
+	'id' => '<?php echo $this->class2id($this->modelClass); ?>-form',
+	'enableAjaxValidation' => <?php echo $ajax; ?>,
+));
+<?php echo '?>'; ?>
+
+
+	<p class="note">
+		<?php echo "<?php echo Yii::t('app', 'Fields with'); ?> <span class=\"required\">*</span> <?php echo Yii::t('app', 'are required'); ?>"; ?>.
+	</p>
+
+	<?php echo "<?php echo \$form->errorSummary(\$model); ?>\n"; ?>
+
+<?php foreach ($this->tableSchema->columns as $column): ?>
+<?php if (!$column->autoIncrement): ?>
+<?php
+                if ($column->isForeignKey) {
+                    $relatedModelClass = $this->findRelation($this->modelClass, $column)[3];
+                    echo "\t\t<?php //BeginFeature:{$relatedModelClass} ?>\n";
+                }
+?>
+		<div class="row">
+		<?php echo "<?php echo " . $this->generateActiveLabel($this->modelClass, $column) . "; ?>\n"; ?>
+		<?php echo "<?php " . $this->generateActiveField($this->modelClass, $column) . "; ?>\n"; ?>
+		<?php echo "<?php echo \$form->error(\$model,'{$column->name}'); ?>\n"; ?>
+		</div><!-- row -->
+<?php
+                if ($column->isForeignKey) {            
+                    echo "\t\t<?php //EndFeature:{$relatedModelClass} ?>\n";
+                }
+?>
+<?php endif; ?>
+<?php endforeach; ?>
+
+<?php echo "<?php
+echo GxHtml::submitButton(Yii::t('app', 'Save'));
+\$this->endWidget();
+?>\n"; ?>
+</div><!-- form -->
---
+++ protected/extensions/giix-core/giixCrud/templates/default/view.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The following variables are available in this template:
+ * - $this: the CrudCode object
+ */
+?>
+<?php
+echo "<?php\n
+\$this->breadcrumbs = array(
+	\$model->label(2) => array('index'),
+	GxHtml::valueEx(\$model),
+);\n";
+?>
+
+$this->menu=array(
+	array('label'=>Yii::t('app', 'List') . ' ' . $model->label(2), 'url'=>array('index')),
+	array('label'=>Yii::t('app', 'Create') . ' ' . $model->label(), 'url'=>array('create')),
+	array('label'=>Yii::t('app', 'Update') . ' ' . $model->label(), 'url'=>array('update', 'id' => $model-><?php echo $this->tableSchema->primaryKey; ?>)),
+	array('label'=>Yii::t('app', 'Delete') . ' ' . $model->label(), 'url'=>'#', 'linkOptions' => array('submit' => array('delete', 'id' => $model-><?php echo $this->tableSchema->primaryKey; ?>), 'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>Yii::t('app', 'Manage') . ' ' . $model->label(2), 'url'=>array('admin')),
+);
+?>
+
+<h1><?php echo '<?php'; ?> echo Yii::t('app', 'View') . ' ' . GxHtml::encode($model->label()) . ' ' . GxHtml::encode(GxHtml::valueEx($model)); ?></h1>
+
+<?php echo '<?php'; ?> $this->widget('zii.widgets.CDetailView', array(
+	'data' => $model,
+	'attributes' => array(
+<?php
+foreach ($this->tableSchema->columns as $column)
+		echo $this->generateDetailViewAttribute($this->modelClass, $column) . ",\n";
+?>
+	),
+));

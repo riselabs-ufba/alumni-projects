419152434e1b52488ce05f1a79222d83febb3968
fosales <fosales@gmail.com>
2017-02-21 22:56:14 -0300

    [Main] Database - Adjustment in schema cache configuration.

--- protected/config/database.php
+++ protected/config/database.php
@@ -3,6 +3,7 @@
 // This is the database connection configuration.
 return array(
 	'connectionString' => 'mysql:host=localhost;dbname=travelnet',
+        'schemaCachingDuration' => 3600,
 	'emulatePrepare' => true,
 	'username' => 'root',
 	'password' => '',

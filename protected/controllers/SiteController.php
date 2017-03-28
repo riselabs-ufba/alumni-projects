a3fd311a3cc9a17c2c717a4e1f0c570e1587d56a
fosales <fosales@gmail.com>
2017-03-28 09:31:30 -0300

    Added script to force users to log in.

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -27,6 +27,9 @@ class SiteController extends Controller
 	 */
 	public function actionIndex()
 	{
+            if (Yii::app()->user->isGuest){
+                Yii::app()->user->loginRequired();
+            }
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
 		$this->render('index');

27dcbad338668f41032e2c33854aeae702cdc148
fosales <fosales@gmail.com>
2017-04-03 22:51:29 -0300

    Extension htmltable added.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $yii=dirname(__FILE__).'/../../../../home/user/workspace/yii-1.1.17/framework/yi
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+//defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 

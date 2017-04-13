adb4b3d3739be6304e3ec49b50cc55f719109c41
Daniel Amador dos Santos <daniel.amador@email.com>
2017-04-13 16:44:43 -0300

    Old passage_selling_spl folder removed.

--- passage_selling_spl/protected/yiic.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// change the following paths if necessary
-$yiic=dirname(__FILE__).'/../../../../../home/user/workspace/yii-1.1.17/framework/yiic.php';
-$config=dirname(__FILE__).'/config/console.php';
-
-require_once($yiic);
--- passage_selling_spl/themes/shadow_dancer/views/layouts/xml.php
+++
@@ -1,6 +0,0 @@
-<?php header("Content-type: text/xml"); /* BeginFeature:XML */ ?>
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'?>
-<Ticket>         
-<?php echo $content; ?>
-</Ticket>
-<?php /* EndFeature:XML */
